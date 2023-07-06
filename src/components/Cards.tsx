eb838d78fc68cce397960a20ddd7ea087affa2e3
igorraknorwid <igorrakfed@gmail.com>
2023-07-06 11:03:16 +0200

    cards location fault research 12

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -13,24 +13,23 @@ function Cards() {
   const searchParams = new URLSearchParams(location.search);
   const year = searchParams.get("year");
   console.log("year", year);
-  const [data, setData] = React.useState<ICard[] | null>(null);
+  const [data] = React.useState<ICard[] | null>(null);
   console.log("year", data);
-  const [isError, setIsError] = React.useState<boolean>(false);
+  const [isError] = React.useState<boolean>(false);
 
   React.useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const query = `*[_type == "card" && '${year}' in years[]->title]{ _id, title }`;
-        const result = await client.fetch<ICard[]>(query);
-        setData(result);
-      } catch (error) {
-        setIsError(true);
-        console.error("Error fetching data from Sanity:", error);
-      }
-    };
-
-    fetchData();
-  }, [year]);
+    // const fetchData = async () => {
+    //   try {
+    //     const query = `*[_type == "card" && '${year}' in years[]->title]{ _id, title }`;
+    //     const result = await client.fetch<ICard[]>(query);
+    //     setData(result);
+    //   } catch (error) {
+    //     setIsError(true);
+    //     console.error("Error fetching data from Sanity:", error);
+    //   }
+    // };
+    // fetchData();
+  }, []);
 
   if (!data) return <div>...LOADING</div>;
   if (isError) return <div>Error fetching data from Sanity!</div>;

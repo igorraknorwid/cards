3cbde72020f82ab59802a674f6567946ab02cde2
igorraknorwid <igorrakfed@gmail.com>
2023-07-06 10:21:51 +0200

    cards location handling

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import client from "../sanityClient";
+import { useLocation } from "react-router-dom";
 import "../index.css";
 
 interface ICard {
@@ -8,15 +9,18 @@ interface ICard {
 }
 
 function Cards() {
+  const location = useLocation();
   const [data, setData] = React.useState<ICard[] | null>(null);
   const [isError, setIsError] = React.useState<boolean>(false);
-
+  const searchParams = new URLSearchParams(location.search);
+  const year = searchParams.get("year");
   React.useEffect(() => {
     const fetchData = async () => {
       try {
-        const query = `*[_type == "card"]{ _id,
-          title,
-        }`; // Replace 'YOUR_DOCUMENT_TYPE' with your actual Sanity document type
+        console.log("year", year);
+        // const query = `*[_type == "card"&& '1946' in years[]->title]{ _id,title,}`;
+        const query = `*[_type == "card" && '${year}' in years[]->title]{ _id, title }`;
+        // Replace 'YOUR_DOCUMENT_TYPE' with your actual Sanity document type
         const result = await client.fetch<ICard[]>(query);
         setData(result);
       } catch (error) {
@@ -32,7 +36,7 @@ function Cards() {
 
   if (!data) return <div>...LOADING</div>;
   if (isError) return <div>"Error fetching data from Sanity:"</div>;
-  return <div>Kartki</div>;
+  return <div>Rok {year}</div>;
 }
 
 export default Cards;

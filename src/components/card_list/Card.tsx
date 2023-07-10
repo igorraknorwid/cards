21adbdfccb25d155daa2b8705009dca2b8108b88
igorraknorwid <igorrakfed@gmail.com>
2023-07-10 10:40:17 +0200

    modal

--- src/components/card_list/Card.tsx
+++ src/components/card_list/Card.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { ICard } from "../../types/card";
+import Modal from "./Modal";
 
 interface ICardComponent {
   card: ICard;
 }
 
 function Card({ card }: ICardComponent) {
+  const [isModal, setIsModal] = React.useState(false);
+  const expandClickHandler = () => {
+    setIsModal((s) => !s);
+  };
   return (
-    <li key={card._id}>
-      <img src={card.image_slug} alt={card.title} height={300} />
+    <li key={card._id} className='p-4 bg-slate-900 text-white border md:w-1/2 '>
+      <img src={card.image_slug} alt={card.title} width={300} />
       <div>
         {card.title}-{card.theme.title}
       </div>
+      <button onClick={expandClickHandler}>Expand</button>
+      {isModal && <Modal card={card} expandClickHandler={expandClickHandler} />}
     </li>
   );
 }
---
+++ src/components/card_list/Modal.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ICard } from "../../types/card";
+
+interface IModal {
+  card: ICard;
+  expandClickHandler: () => void;
+}
+
+function Modal({ card, expandClickHandler }: IModal) {
+  return (
+    <div className='fixed top-0 bottom-0 right-0 left-0 bg-black/80 flex flex-col justify-center items-center'>
+      <button className='border' onClick={expandClickHandler}>
+        Close
+      </button>
+      <img src={card.image_slug} alt={card.title} width={1000} />
+    </div>
+  );
+}
+
+export default Modal;
